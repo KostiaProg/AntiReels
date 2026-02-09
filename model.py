3f3579554b168fdc2a4c8b86e59a7aa8bdc3c880
Kostiantyn <kostjasolovej07@gmail.com>
2026-02-09 10:25:57 +0200

    model code

--- model.py
+++ model.py
@@ -1,25 +1,46 @@
 from ultralytics import YOLO
+from ultralytics.utils.plotting import plot_results
+
 import os
+from torch.cuda import is_available
 
 # path
-DATA_PATH = "data_yolo26/data.yaml"
+DATA_PATH = "data_yolo26"
+DATA_SETTINGS_PATH = DATA_PATH + "/data.yaml"
+TRAIN_PATH = DATA_PATH + "/train"
+VAL_PATH = DATA_PATH + "/valid"
+
 MODEL_PATH = "models"
-MODEL_NAME = "phone_detection_yolo26_v1.0.pt"
+MODEL_NAME = "phone_detection_yolo26_v1.0.engine" if is_available() else "phone_detection_yolo26_v1.0.pt"
 
 # training
-def train(data_path: str = DATA_PATH, model_path: str = MODEL_PATH, model_name: str = MODEL_NAME):
+def train(data_path: str = DATA_SETTINGS_PATH, train_path: str = TRAIN_PATH, val_path: str = VAL_PATH, model_name: str = MODEL_NAME):
     model = YOLO("yolo26b.pt")
     device = 0 # os.cpu_count()
 
-    results = model.train(data=data_path, epochs=100, imgsz=512, batch=20, device=device, name=model_name)
-    val = model.val()
+    model.train(data=data_path, epochs=100, imgsz=512, batch=20, device=device, name=model_name)
+    plot_results(train_path)
 
-    model.export(format="TensorRT")
+    model.val()
+    plot_results(val_path)
+
+    format = "engine" if is_available() else "onnx"
+    model.export(format=format)
     # model.save()
 
-def get_phones(img, model_path: str = MODEL_PATH, model_name: str = MODEL_NAME):
+# if phone detected - true
+def get_phones(img, model_path: str = MODEL_PATH, model_name: str = MODEL_NAME) -> bool:
     # pretrain model if didn't do it before
-    if not os.path.exists(model_path + "/" + model_name):
+    full_model_path = model_path + "/" + model_name
+    if not os.path.exists(full_model_path):
         train()
     
-    # get model and get phone
+    # process img
+    model = YOLO(full_model_path)
+    img_results = model(img)
+
+    for name in img_results[0].names:
+        if name == "phone":
+            return True
+    return False
+    # return True if len(img_results[0]) > 0 else False
