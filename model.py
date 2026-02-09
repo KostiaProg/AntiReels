faa2002c21f60a52b46610c3c084d64f3a870117
Kostiantyn <kostjasolovej07@gmail.com>
2026-02-09 11:36:42 +0200

    errors in model fix

--- model.py
+++ model.py
@@ -11,14 +11,15 @@ TRAIN_PATH = DATA_PATH + "/train"
 VAL_PATH = DATA_PATH + "/valid"
 
 MODEL_PATH = "models"
-MODEL_NAME = "phone_detection_yolo26_v1.0.engine" if is_available() else "phone_detection_yolo26_v1.0.pt"
+MODEL_NAME = "phone_detection_yolo26_v1.0"
+MODEL_FULL_NAME = MODEL_PATH + "/" + MODEL_NAME + ".engine" if is_available() else MODEL_PATH + MODEL_PATH + "/" + MODEL_NAME + ".pt"
 
 # training
 def train(data_path: str = DATA_SETTINGS_PATH, train_path: str = TRAIN_PATH, val_path: str = VAL_PATH, model_name: str = MODEL_NAME):
-    model = YOLO("yolo26b.pt")
-    device = 0 # os.cpu_count()
+    model = YOLO("yolo26m.pt")
+    device = 0 if is_available() else "cpu"
 
-    model.train(data=data_path, epochs=100, imgsz=512, batch=20, device=device, name=model_name)
+    model.train(data=data_path, epochs=50, imgsz=512, batch=20, device=device, name=model_name)
     plot_results(train_path)
 
     model.val()
@@ -29,14 +30,13 @@ def train(data_path: str = DATA_SETTINGS_PATH, train_path: str = TRAIN_PATH, val
     # model.save()
 
 # if phone detected - true
-def get_phones(img, model_path: str = MODEL_PATH, model_name: str = MODEL_NAME) -> bool:
+def get_phones(img, model_path: str = MODEL_PATH, model_full_name: str = MODEL_FULL_NAME) -> bool:
     # pretrain model if didn't do it before
-    full_model_path = model_path + "/" + model_name
-    if not os.path.exists(full_model_path):
+    if not os.path.exists(model_full_name):
         train()
     
     # process img
-    model = YOLO(full_model_path)
+    model = YOLO(model_full_name)
     img_results = model(img)
 
     for name in img_results[0].names:
